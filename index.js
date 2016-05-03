f20ba098986a91e87f2014256ae18287883251ad
P L Garlaschi Sousa <pedro@ps-macbook-pro.home>
2016-05-03 12:56:37 +0100

    fix express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 //app.use("/css", express.static(__dirname + '/www/css'));
 //app.use("/js", express.static(__dirname + '/www/js'));
+app.use("/", express.static(__dirname + '/www'));
 app.use("/images", express.static(__dirname + '/www/images'));
 
 var http = require('http').Server(app);
